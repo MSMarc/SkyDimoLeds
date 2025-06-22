0524188c3fadfeb93aec66ab0482fc2aae2514a9
Marc <113135537+MSMarc@users.noreply.github.com>
2025-06-22 13:06:16 +0200

    Init

---
+++ skydimo.py
@@ -0,0 +1,30 @@
+import serial
+import time
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+NUM_LEDS = 65
+PORT = 'COM3'
+BAUD_RATE = 115200
+
+ser = serial.Serial(PORT, BAUD_RATE)
+
+prefix = b'Ada' + bytes([
+    (NUM_LEDS >> 8) & 0xFF,
+    NUM_LEDS & 0xFF,
+    ((NUM_LEDS >> 8) & 0xFF) ^ (NUM_LEDS & 0xFF)
+])
+
+led_data = [255, 255, 255] * NUM_LEDS
+frame = prefix + bytes(led_data)
+
+try:
+    while True:
+        ser.write(frame)
+        time.sleep(1)
+except KeyboardInterrupt:
+    off_data = [0, 0, 0] * NUM_LEDS
+    ser.write(prefix + bytes(off_data))
+    ser.close()
